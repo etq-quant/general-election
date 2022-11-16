5d2da229bbb427198c3339e62996638de39775de
ngkeanchewet <ngkeanchewet@gmail.com>
2022-11-16 14:29:55 +0800

    split by state support

--- app.py
+++ app.py
@@ -25,6 +25,41 @@ def load_data_GE15():
     return df
 
 
+party_color = {"BN": "#2B65EC", "PH": "#E41B17", "PN": "#659EC7"}
+
+
+def highlight_party(val):
+    return "background-color: {}".format(party_color.get(val))
+
+
+def get_party(x):
+    PN_LIST = ["PR-PAS", "GS-PAS", "PH-PPBM", "BA-PAS"]
+    PH_LIST = ["PH-DAP", "PH-PKR", "PH-PAN", "PR-DAP", "PR-PKR", "BA-PKR"]
+    if x in PN_LIST:
+        return "PN"
+    if x in PH_LIST:
+        return "PH"
+    x = x.split("-")[0]
+    if x == "BN":
+        return "BN"
+    else:
+        return None
+
+
+pdf = pd.read_csv("data/malay_parameters.csv")
+para_data = {}
+for k, v in pdf.set_index("state").iterrows():
+    para_data[k] = v.to_dict()
+
+races = ["Malay", "Chinese", "Indian", "Others"]
+
+GE14_voter_cols = [
+    "GE14_{}_ratio".format(i) for i in ["malay", "chinese", "indian", "others"]
+]
+GE15_voter_cols = [
+    "GE15_{}_ratio".format(i) for i in ["malay", "chinese", "indian", "others"]
+]
+
 adf = load_data()
 df = adf[~adf["state"].isin(["Sarawak", "Sabah"])].copy()
 gdf = load_data_GE15()
@@ -46,49 +81,59 @@ tab_estimated_result, tab_descriptive_analysis, tab_age_group = st.tabs(
     ["Estimated Result", "Descriptive Analysis", "Age Group Analysis"]
 )
 
-with tab_estimated_result:
+
+def generate_state(df, state):
     data = {}
     # with v2_tab:
-    my_expander = st.expander(label="Turnout", expanded=True)
+    my_expander = st.expander(label="Turnout", expanded=False)
     with my_expander:
-        para = [("Malay", 72), ("Chinese", 70), ("Indian", 70), ("Others", 70)]
+        para = [("Malay", 80), ("Chinese", 86), ("Indian", 88), ("Others", 80)]
         st_cols = st.columns(len(para))
         for k, v in enumerate(st_cols):
-            data["{}_turnout".format(para[k][0])] = v.slider(
-                "{} Turnout".format(para[k][0]), 0, 100, para[k][1]
+            data["{}_{}_turnout".format(state, races[k])] = v.slider(
+                "{} {} Turnout".format(state, races[k]), 0, 100, para[k][1]
             )
 
     my_expander = st.expander(label="BN Support", expanded=True)
     with my_expander:
-        para = [("Malay", 38), ("Chinese", 20), ("Indian", 30), ("Others", 20)]
+        para = [("Malay", 27), ("Chinese", 15), ("Indian", 25), ("Others", 40)]
         st_cols = st.columns(len(para))
         for k, v in enumerate(st_cols):
-            data["BN_{}_support".format(para[k][0])] = v.slider(
-                "{} Support".format(para[k][0]), 0, 100, para[k][1]
+            data["{}_BN_{}_support".format(state, races[k])] = v.slider(
+                "{} {} Support".format(state, races[k]),
+                0,
+                100,
+                int(para_data[state]["BN_{}_support".format(races[k])]),
             )
 
     my_expander = st.expander(label="PH Support", expanded=True)
     with my_expander:
-        para = [("Malay", 25), ("Chinese", 60), ("Indian", 60), ("Others", 20)]
+        para = [("Malay", 19), ("Chinese", 80), ("Indian", 70), ("Others", 30)]
         st_cols = st.columns(len(para))
         for k, v in enumerate(st_cols):
-            data["PH_{}_support".format(para[k][0])] = v.slider(
-                "PH {} Support".format(para[k][0]), 0, 100, para[k][1]
+            data["{}_PH_{}_support".format(state, races[k])] = v.slider(
+                "{} PH {} Support".format(state, races[k]),
+                0,
+                100,
+                int(para_data[state]["PH_{}_support".format(races[k])]),
             )
 
     my_expander = st.expander(label="PN Support", expanded=True)
     with my_expander:
-        para = [("Malay", 25), ("Chinese", 15), ("Indian", 10), ("Others", 20)]
+        para = [("Malay", 31), ("Chinese", 5), ("Indian", 5), ("Others", 30)]
         st_cols = st.columns(len(para))
         for k, v in enumerate(st_cols):
-            data["PN_{}_support".format(para[k][0])] = v.slider(
-                "PN {} Support".format(para[k][0]), 0, 100, para[k][1]
+            data["{}_PN_{}_support".format(state, races[k])] = v.slider(
+                "{} PN {} Support".format(state, races[k]),
+                0,
+                100,
+                int(para_data[state]["PN_{}_support".format(races[k])]),
             )
 
     my_expander = st.expander(label="Safe Threshold", expanded=False)
     with my_expander:
-        data["safe_threshold"] = st.slider(
-            "Safe Threshold: percentage of majority - percentage of new voters increase",
+        data[f"{state}_safe_threshold"] = st.slider(
+            f"{state} Safe Threshold: percentage of majority - percentage of new voters increase",
             -100,
             100,
             -100,
@@ -96,50 +141,31 @@ with tab_estimated_result:
 
     my_expander = st.expander(label="Safe Combo", expanded=False)
     with my_expander:
-        data["safe_combo"] = st.multiselect(
-            "Safe Combo",
-            options=["2004", "2008", "2013", "2018"],
-            default=["2013", "2018"],
+        data[f"{state}_safe_combo"] = st.multiselect(
+            f"{state} Safe Combo", options=["2004", "2008", "2013", "2018"], default=[],
         )
 
     for i in ["BN", "PH", "PN"]:
         df[i] = (
-            data["Malay_turnout"]
-            * data[f"{i}_Malay_support"]
+            data[f"{state}_Malay_turnout"]
+            * data[f"{state}_{i}_Malay_support"]
             * df["GE15_malay_voters"]
             / 10000
-            + data["Chinese_turnout"]
-            * data[f"{i}_Chinese_support"]
+            + data[f"{state}_Chinese_turnout"]
+            * data[f"{state}_{i}_Chinese_support"]
             * df["GE15_chinese_voters"]
             / 10000
-            + data["Indian_turnout"]
-            * data[f"{i}_Indian_support"]
+            + data[f"{state}_Indian_turnout"]
+            * data[f"{state}_{i}_Indian_support"]
             * df["GE15_indian_voters"]
             / 10000
-            + data["Others_turnout"]
-            * data[f"{i}_Others_support"]
+            + data[f"{state}_Others_turnout"]
+            * data[f"{state}_{i}_Others_support"]
             * df["GE15_others_voters"]
             / 10000
         )
 
     df["estimate_party"] = df[["BN", "PH", "PN"]].idxmax(axis=1)
-    party_color = {"BN": "#2B65EC", "PH": "#E41B17", "PN": "#659EC7"}
-
-    def highlight_party(val):
-        return "background-color: {}".format(party_color.get(val))
-
-    def get_party(x):
-        PN_LIST = ["PR-PAS", "GS-PAS", "PH-PPBM", "BA-PAS"]
-        PH_LIST = ["PH-DAP", "PH-PKR", "PH-PAN", "PR-DAP", "PR-PKR", "BA-PKR"]
-        if x in PN_LIST:
-            return "PN"
-        if x in PH_LIST:
-            return "PH"
-        x = x.split("-")[0]
-        if x == "BN":
-            return "BN"
-        else:
-            return None
 
     df["safe_threshold"] = (
         df["GE14_majority_pct"] - df["registered_voters_increase_pct"]
@@ -148,10 +174,14 @@ with tab_estimated_result:
     for i in ["2004", "2008", "2013", "2018"]:
         df[f"{i}_party"] = df[i].map(get_party)
 
-    if data["safe_combo"]:
-        df["base"] = df[[f"{i}_party" for i in data["safe_combo"]]].nunique(axis=1) == 1
-        df["base"] = df["base"] & (df["safe_threshold"] >= data["safe_threshold"])
-        sname = "{}_party".format(data["safe_combo"][-1])
+    if data[f"{state}_safe_combo"]:
+        df["base"] = (
+            df[[f"{i}_party" for i in data[f"{state}_safe_combo"]]].nunique(axis=1) == 1
+        )
+        df["base"] = df["base"] & (
+            df["safe_threshold"] >= data[f"{state}_safe_threshold"]
+        )
+        sname = "{}_party".format(data[f"{state}_safe_combo"][-1])
         df["base_party"] = df[["base", sname]].apply(
             lambda x: x[sname] if x["base"] else None, axis=1,
         )
@@ -159,14 +189,10 @@ with tab_estimated_result:
         df["base"] = False
         df["base_party"] = None
 
-    df["party"] = df["base_party"].fillna(df["estimate_party"])
+    for k, v in hard_base_party.items():
+        df.loc[df["constituency"].isin(v), "base_party"] = k
 
-    GE14_voter_cols = [
-        "GE14_{}_ratio".format(i) for i in ["malay", "chinese", "indian", "others"]
-    ]
-    GE15_voter_cols = [
-        "GE15_{}_ratio".format(i) for i in ["malay", "chinese", "indian", "others"]
-    ]
+    df["party"] = df["base_party"].fillna(df["estimate_party"])
 
     int_types = ["int16", "int32", "int64"]
     float_types = ["float16", "float32", "float64"]
@@ -175,10 +201,10 @@ with tab_estimated_result:
     tab_chart, tab_data, tab_raw_data = st.tabs(["Chart", "Data", "Raw Data"])
 
     with tab_chart:
-        st.subheader("Estimated Result Excluded Sabah and Sarawak")
+        st.subheader(f"Estimated {state} Result")
         sdf = df.groupby(["party"]).size().reset_index().rename(columns={0: "seats"})
         chart = (
-            alt.Chart(sdf, title="Estimated Result")
+            alt.Chart(sdf, title=f"Estimated {state} Result")
             .mark_bar()
             .encode(
                 alt.X("party"),
@@ -303,6 +329,156 @@ with tab_estimated_result:
             height=800,
         )
 
+        return df
+
+
+def run_state(df, state, tab):
+    with tab:
+        st.subheader(state)
+        df = generate_state(df, state)
+    return df
+
+
+with tab_estimated_result:
+    hard_base_party = {}
+    hard_base_party["BN"] = st.multiselect(
+        "Hard Rule BN",
+        df["constituency"].unique(),
+        default=["Sungai Buloh (previously known as Subang)[3]"],
+    )
+    hard_base_party["PH"] = st.multiselect(
+        "Hard Rule PH", df["constituency"].unique(), default=["Muar"]
+    )
+    hard_base_party["PN"] = st.multiselect(
+        "Hard Rule PN", df["constituency"].unique(), default=["Arau"]
+    )
+
+    undecided_malay = {}
+    undecided_malay["BN"] = st.slider("undecided Malays to BN", 0, 100, 30, 5)
+    undecided_malay["PH"] = st.slider("undecided Malays to PN", 0, 100, 30, 5)
+    undecided_malay["PN"] = st.slider("undecided Malays to PN", 0, 100, 40, 5)
+
+    for state in para_data.keys():
+        for party in undecided_malay.keys():
+            para_data[state][f"{party}_Malay_support"] = (
+                para_data[state][f"{party}_Malay_support"]
+                + undecided_malay[party] * para_data[state]["unsure"] / 100
+            )
+
+    states = df["state"].unique().tolist()
+    state_tabs = st.tabs(states)
+
+    state_df_list = [
+        run_state(df[df["state"] == state], state, tab)
+        for state, tab in zip(states, state_tabs)
+    ]
+    ndf = pd.concat(state_df_list)
+
+    tab_national_chart, tab_national_data, = st.tabs(
+        ["National Chart", "National Data"]
+    )
+    with tab_national_chart:
+        st.subheader("Estimated National Result")
+        sdf = ndf.groupby(["party"]).size().reset_index().rename(columns={0: "seats"})
+        chart = (
+            alt.Chart(sdf, title=f"Estimated National Result")
+            .mark_bar()
+            .encode(
+                alt.X("party"),
+                alt.Y("seats"),
+                alt.Color(
+                    "party",
+                    scale=alt.Scale(
+                        domain=list(party_color.keys()),
+                        range=list(party_color.values()),
+                    ),
+                ),
+                alt.Tooltip(["party", "seats"]),
+            )
+        )
+
+        text = chart.mark_text(
+            align="center", baseline="bottom", fontSize=24, color="black",
+        ).encode(alt.Text("seats"))
+
+        chart = (
+            (chart + text)
+            .properties(height=400)
+            .configure_title(fontSize=24)
+            .configure_axis(labelFontSize=24, titleFontSize=26)
+        )
+
+        st.altair_chart(chart, use_container_width=True)
+        nsdf = (
+            ndf.groupby(["state", "party"])
+            .size()
+            .reset_index()
+            .pivot_table(index=["state"], columns=["party"])
+        )
+        nsdf.columns = [b for a, b in nsdf.columns]
+        nsdf = nsdf.fillna(0).astype(int)
+        st.dataframe(nsdf, height=530)
+
+    with tab_national_data:
+        front_cols = [
+            "##",
+            "state",
+            "constituency",
+            "2004",
+            "2008",
+            "2013",
+            "2018",
+            "base_party",
+            "estimate_party",
+            "BN",
+            "PH",
+            "PN",
+            "party",
+            "GE14_registered_voters",
+            "GE15_registered_voters",
+            "registered_voters_increase_pct",
+            "GE14_majority_pct",
+            "safe_threshold",
+            "registered_voters_increase",
+            "GE14_majority",
+            "GE14_malay_ratio",
+            "GE15_malay_ratio",
+            "GE14_chinese_ratio",
+            "GE15_chinese_ratio",
+            "GE14_indian_ratio",
+            "GE15_indian_ratio",
+            "GE14_others_ratio",
+            "GE15_others_ratio",
+            "GE14_malay_voters",
+            "GE15_malay_voters",
+            "GE14_chinese_voters",
+            "GE15_chinese_voters",
+            "GE14_indian_voters",
+            "GE15_indian_voters",
+            "GE14_others_voters",
+            "GE15_others_voters",
+        ]
+        int_types = ["int16", "int32", "int64"]
+        float_types = ["float16", "float32", "float64"]
+        num_cols = ndf.select_dtypes(include=int_types + float_types).columns
+        rearrange_cols = front_cols + [i for i in ndf.columns if i not in front_cols]
+        st.dataframe(
+            ndf[rearrange_cols]
+            .set_index(["##", "state", "constituency"])
+            .style.format({i: "{:,.0f}" for i in num_cols})
+            .applymap(highlight_party, subset=["party"])
+            .background_gradient(
+                subset=GE14_voter_cols + GE15_voter_cols, cmap="Blues", axis=1
+            )
+            .background_gradient(
+                subset="registered_voters_increase_pct", cmap="Oranges", axis=0
+            )
+            .background_gradient(subset="GE14_majority_pct", cmap="Greens", axis=0)
+            .background_gradient(subset="safe_threshold", cmap="Blues", axis=0)
+            .highlight_max(subset=["BN", "PH", "PN"], color="#AAF0D1", axis=1),
+            use_container_width=True,
+            height=800,
+        )
 
 with tab_descriptive_analysis:
 
